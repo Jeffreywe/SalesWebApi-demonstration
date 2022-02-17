9d46a4b0fedd6c7a7dcf36546364a2b5f3a542c7
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-16 23:56:23 -0500

    Added Orders to database and orders controller

--- SalesWebApi/Models/AppDbContext.cs
+++ SalesWebApi/Models/AppDbContext.cs
@@ -9,6 +9,7 @@ namespace SalesWebApi.Models {
 
         public virtual DbSet<Customer> Customers { get; set; } // creates an instance of our Customer class,
                                                                // and identifying it as Customers for the DbContext to use
+        public virtual DbSet<Order> Orders { get; set; }
         //default constructor not needed
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } // creates an instance and gets the options from DbContext parent,
                                                                                         // and sets it up for us to modify
---
+++ SalesWebApi/Models/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebApi.Models {
+    public class Order {
+
+        public int Id { get; set; }
+        [Required, StringLength(80)]
+        public string Description { get; set; }
+        public bool Shipped { get; set; }
+        [Column(TypeName = "decimal(7,2)")]
+        public decimal Total { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+
+        public Order() { }
+    }
+}
